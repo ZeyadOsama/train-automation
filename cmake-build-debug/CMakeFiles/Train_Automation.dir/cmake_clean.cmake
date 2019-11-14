e9e730ae50775f7bc24881c77eee103ad13942c2
ZeyadOsama <zeyad.osama@gmail.com>
2019-11-14 15:18:48 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Train_Automation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Train_Automation.dir/main.c.o"
+  "Train_Automation"
+  "Train_Automation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Train_Automation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
